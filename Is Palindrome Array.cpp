0b0e8bce637038c8e4e7f0c379308bf2eafb87a3
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2022-12-17 13:15:53 +0200

    Create Is Palindrome Array.cpp

---
+++ Is Palindrome Array.cpp
@@ -0,0 +1,38 @@
+/*
+Read N, then N integers for an Array. Call a function with the array to check if
+the array is palindrome or note
+○ We already coded it before
+○ Just copy code and rearrange to call function with array
+*/
+#include<iostream>
+using namespace std;
+
+int read_array(int arr[]) 
+{
+	int len;
+	cin >> len;
+	for (int i = 0; i < len; ++i)
+		cin >> arr[i];
+
+	return len;
+}
+
+bool is_palindrome(int arr[], int n) 
+{
+	int st = 0, en = n - 1;
+	while (st < en)
+	{
+		if (arr[st] != arr[en])
+			return false;
+		st++, en--;
+	}
+	return true;
+}
+
+int main()
+{
+	int arr[100];
+	int len = read_array(arr);
+	cout << is_palindrome(arr, len);
+	return 0;
+}
